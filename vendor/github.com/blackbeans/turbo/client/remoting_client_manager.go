e68f083c3e2adcf579de5596efc6731ccbcddbd6
blackbeans <blackbeans.zc@gmail.com>
2016-06-30 11:51:08 +0800

    bugfix：修复remoteclient内的重连死锁 	modified:   Godeps/Godeps.json 	modified:   vendor/github.com/blackbeans/turbo/client/remoting_client_manager.go

--- vendor/github.com/blackbeans/turbo/client/remoting_client_manager.go
+++ vendor/github.com/blackbeans/turbo/client/remoting_client_manager.go
@@ -187,6 +187,7 @@ func (self *ClientManager) FindRemoteClient(hostport string) *RemotingClient {
 func (self *ClientManager) FindRemoteClients(groupIds []string, filter func(groupId string, rc *RemotingClient) bool) map[string][]*RemotingClient {
 	clients := make(map[string][]*RemotingClient, 10)
 	var closedClients []*RemotingClient
+	self.lock.RLock()
 	for _, gid := range groupIds {
 		if len(self.groupClients[gid]) <= 0 {
 			continue
@@ -197,7 +198,6 @@ func (self *ClientManager) FindRemoteClients(groupIds []string, filter func(grou
 			gclient = make([]*RemotingClient, 0, 10)
 		}
 
-		self.lock.RLock()
 		for _, c := range self.groupClients[gid] {
 
 			if c.IsClosed() {
@@ -212,10 +212,9 @@ func (self *ClientManager) FindRemoteClients(groupIds []string, filter func(grou
 				gclient = append(gclient, c)
 			}
 		}
-		self.lock.RUnlock()
-
 		clients[gid] = gclient
 	}
+	self.lock.RUnlock()
 
 	//删除掉关掉的clients
 	if nil != closedClients && len(closedClients) > 0 {
